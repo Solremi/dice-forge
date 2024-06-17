cc6076e733add04d4d3413bb0e80e02da0c8b5e3
solremi <jeremie.roc@gmail.com>
2024-06-17 11:56:18 +0200

    maj

--- client/axios/axios.ts
+++ client/axios/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 
-const baseURL = process.env.REACT_APP_API_URL;
+const baseURL = (import.meta as any).env.REACT_APP_API_URL;
 console.log('API URL:', baseURL);
 
 const axiosInstance = axios.create({
