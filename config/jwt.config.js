74e1c8cf8602898c15ed5f2e27304e09d3feaa8d
solremi <jeremie.roc@gmail.com>
2024-06-21 09:17:22 +0200

    maj

--- config/jwt.config.js
+++ config/jwt.config.js
@@ -1,9 +1,11 @@
 const jwtConfig = {
     secretKey: process.env.JWT_SECRET,
     options: {
-        accessExpiresIn: '15m', // Durée de vie du token d'authentification
-        refreshExpiresIn: '1d' // Durée de vie du token de rafraîchissement 
+        algorithm: 'HS256', 
+        accessExpiresIn: '15m', 
+        refreshExpiresIn: '1d'
     }
 };
 
 export default jwtConfig;
+
--- index.js
+++ index.js
@@ -35,7 +35,10 @@ app.use(
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
-    cookie: { secure: false, maxAge: 1000 * 60 * 60 * 24 },
+    cookie: {
+      secure: process.env.NODE_ENV === 'production', 
+      maxAge: 1000 * 60 * 60 * 24
+    }
   })
 );
 
